c442b73850b85041f5fcbae9e711313f868bed9a
ugonna-onyejekwe <innocentugonna063@gmail.com>
2024-06-05 19:05:20 +0100

    Added author image and edited few things

--- src/components/authur-section/index.jsx
+++ src/components/authur-section/index.jsx
@@ -1,19 +1,45 @@
 import "./style.scss";
 import { FaFacebookF, FaWhatsapp } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
-import authur_image from "../../assets/authur.jpg";
+import author_image1 from "../../assets/author1.jpg";
+import author_image2 from "../../assets/author2.jpeg";
+
+// import Swiper core and required modules
+import { Autoplay } from "swiper/modules";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/autoplay";
 
 export const Authur_section = () => {
   return (
     <>
       <section className="authur_section" id="authur">
         <div className="container">
-          <div className="img_con">
-            <img src={authur_image} alt="authur image" />
-          </div>
-
           <div className="txt_con">
-            <h2>about authur</h2>
+            <h2>about author</h2>
+
+            <div className="img_wrapper">
+              <Swiper
+                modules={[Autoplay]}
+                spaceBetween={20}
+                slidesPerView={1}
+                autoplay={{ delay: 9000 }}
+                speed={1000}
+                // loop={true}
+                className="img_con"
+              >
+                <SwiperSlide className="img">
+                  <img src={author_image1} alt="author image" />
+                </SwiperSlide>
+                <SwiperSlide className="img">
+                  <img src={author_image2} alt="author image" />
+                </SwiperSlide>
+              </Swiper>
+            </div>
+
             <p>
               Victor Greene (Echezona) is a young digital entrepreneur, author,
               Thought leader and a Brand psychologist for two reputable firms in
