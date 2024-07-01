9432087deeba7116aca9134787b8a30f73580de0
ugonna-onyejekwe <innocentugonna063@gmail.com>
2024-07-01 16:11:51 +0100

    config changes for vercel web analystics

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+// import { inject } from "@vercel/analytics";
+import { Analytics } from "@vercel/analytics/react";
+
+// inject();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
+      <Analytics />
       <App />
     </BrowserRouter>
   </React.StrictMode>
